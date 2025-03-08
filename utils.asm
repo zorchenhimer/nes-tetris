109d3798b7b035de4364f6f88c0370a248eede53
Zorchenhimer <zorchenhimer@gmail.com>
2025-03-08 14:12:25 -0500

    Remove ButtonPressedP2 & ButtonReleasedP2
    These two functions were never used and the original ButtonPressed &
    ButtonReleased routines are player-aware.

--- utils.asm
+++ utils.asm
@@ -66,32 +66,6 @@ ButtonPressed:
     lda #1
     rts
 
-;; Was a button pressed this frame?
-;ButtonPressedP2:
-;    sta TmpX
-;    and Controller+1
-;    sta TmpY
-;
-;    lda Controller_Old+1
-;    and TmpX
-;
-;    cmp TmpY
-;    bne @btnPress_stb
-;
-;    ; no button change
-;    rts
-;
-;@btnPress_stb:
-;    ; button released
-;    lda TmpY
-;    bne @btnPress_stc
-;    rts
-;
-;@btnPress_stc:
-;    ; button pressed
-;    lda #1
-;    rts
-
 ; PlayerID in Y
 ButtonReleased:
     sta TmpY
@@ -106,20 +80,6 @@ ButtonReleased:
     and TmpY
     eor TmpX
     rts
-
-;ButtonReleasedP2:
-;    sta TmpY
-;    and Controller_Old+1
-;    bne :+
-;    ; wasn't pressed last frame, can't be
-;    ; released this frame
-;    rts
-;:
-;    sta TmpX
-;    lda Controller+1
-;    and TmpY
-;    eor TmpX
-;    rts
 
 ; Palette data address in AddressPointer1
 ; Palette index in X
